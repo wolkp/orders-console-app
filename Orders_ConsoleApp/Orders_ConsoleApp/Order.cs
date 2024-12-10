2fd42fc0ceb22b24dd7c77dfd3b9a02cc9df41c1
pwolk <wolkpiotr95@gmail.com>
2024-12-10 16:55:46 +0100

    Add Order class to handle adding and removing products and calculate the total price of order

---
+++ Orders_ConsoleApp/Orders_ConsoleApp/Order.cs
@@ -0,0 +1,38 @@
+﻿namespace OrderApp
+{ 
+    public class Order
+    {
+        private readonly List<OrderItem> _items = new();
+
+        public void AddProduct(Product product, int quantity)
+        {
+            var existingItem = _items.FirstOrDefault(i => i.Product.Name == product.Name);
+
+            if(existingItem != null)
+            {
+                existingItem.Quantity += quantity;
+            }
+            else
+            {
+                var newItem = new OrderItem(product, quantity);
+                _items.Add(newItem);
+            }
+        }
+
+        public void RemoveProduct(string productName)
+        {
+            var itemToRemove = _items.FirstOrDefault(i => i.Product.Name == productName);
+
+            if (itemToRemove != null)
+            {
+                _items.Remove(itemToRemove);
+            }
+        }
+
+        public decimal CalculateTotalPrice()
+        {
+            var totalPrice = _items.Sum(i => i.GetTotalPrice());
+            return totalPrice;
+        }
+    }
+}
