6095fafb808e21e69a68ffe35e3d8f7cd8c2fe59
pwolk <wolkpiotr95@gmail.com>
2024-12-10 16:41:26 +0100

    Add OrderItem class to represent items in the order

---
+++ Orders_ConsoleApp/Orders_ConsoleApp/OrderItem.cs
@@ -0,0 +1,19 @@
+﻿namespace OrderApp
+{
+    public class OrderItem
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+
+        public OrderItem(Product product, int quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+        }
+
+        public decimal GetTotalPrice()
+        {
+            return Product.Price * Quantity;
+        }
+    }
+}
